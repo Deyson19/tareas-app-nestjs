288f0f1b5d3b504886d2b2c45934027828e085fe
Deyson Venté <deiandresvent@hotmail.com>
2024-06-10 18:06:25 -0500

    Lógica del controlador implementada.

--- src/tareas/tareas.controller.ts
+++ src/tareas/tareas.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { TareasService } from './tareas.service';
 import { CreateTareaDto } from './dto/create-tarea.dto';
 import { UpdateTareaDto } from './dto/update-tarea.dto';
@@ -6,10 +14,19 @@ import { UpdateTareaDto } from './dto/update-tarea.dto';
 @Controller('tareas')
 export class TareasController {
   constructor(private readonly tareasService: TareasService) {}
-
+  private result: object;
   @Post()
   create(@Body() createTareaDto: CreateTareaDto) {
     return this.tareasService.create(createTareaDto);
+  }
+  @Post('addRange')
+  createRange(@Body() crearTareas: CreateTareaDto[]) {
+    crearTareas.forEach((x) => {
+      this.result = this.tareasService.create(x);
+    });
+    return {
+      isValid: this.result !== null,
+    };
   }
 
   @Get()
@@ -19,16 +36,16 @@ export class TareasController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.tareasService.findOne(+id);
+    return this.tareasService.findOne(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateTareaDto: UpdateTareaDto) {
-    return this.tareasService.update(+id, updateTareaDto);
+    return this.tareasService.update(id, updateTareaDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.tareasService.remove(+id);
+    return this.tareasService.remove(id);
   }
 }
