505a19dd9da456c74bbe357f66a827b76a100c70
Deyson Venté <deiandresvent@hotmail.com>
2024-06-10 18:05:53 -0500

    Base de datos configurada.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { TareasModule } from './tareas/tareas.module';
 
 @Module({
-  imports: [TareasModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
+    TareasModule,
+  ],
   controllers: [],
   providers: [],
 })
