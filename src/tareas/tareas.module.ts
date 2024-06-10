04ac1d573206a7a0f4fc653777c27fe8c1a85e98
Deyson Venté <deiandresvent@hotmail.com>
2024-06-10 18:06:59 -0500

    Nombre para la tabla de la base de datos configurada, schema para mongoose

--- src/tareas/tareas.module.ts
+++ src/tareas/tareas.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TareasService } from './tareas.service';
 import { TareasController } from './tareas.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tarea, TareaSchema } from './entities/tarea.entity';
 
 @Module({
   controllers: [TareasController],
   providers: [TareasService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([
+      {
+        name: Tarea.name,
+        schema: TareaSchema,
+      },
+    ]),
+  ],
 })
 export class TareasModule {}
